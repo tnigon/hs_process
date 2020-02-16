74ece0ca69354f6bfca29b566fe45f733c4e0c14
tnigon <nigo0024@umn.edu>
2020-02-15 20:50:21 -0600

    gdal try again6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ import sys
 # sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../../'))
 
-autodoc_mock_imports = ['gdal']
+# autodoc_mock_imports = ['gdal']
 
 import sphinx_bootstrap_theme
 import recommonmark
