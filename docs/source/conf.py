7632cd67a3670fc9ca937adc9a88d8fe41df9c49
tnigon <nigo0024@umn.edu>
2020-02-15 20:02:19 -0600

    adding mock imoports to doc conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,8 @@ import sys
 # sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../../'))
 
+autodoc_mock_imports = ['geopandas', 'osgeo', 'seaborn']
+
 import sphinx_bootstrap_theme
 import recommonmark
 from recommonmark.transform import AutoStructify
